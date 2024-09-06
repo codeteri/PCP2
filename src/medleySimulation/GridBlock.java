ecdceb7208cbb7515790d23ee47910321fbc913a
WAVYTERRY <codeeeterry@gmail.com>
2024-09-07 00:07:35 +0200

    Pulling recent commits from the remote repository

--- src/medleySimulation/GridBlock.java
+++ src/medleySimulation/GridBlock.java
@@ -4,64 +4,55 @@
 
 package medleySimulation;
 
-
 public class GridBlock {
-	
-	private int isOccupied; 
-	
-	private final boolean isStart;  //is this a starting block?
-	private int [] coords; // the coordinate of the block.
-	
-	GridBlock(boolean startBlock) throws InterruptedException {
-		isStart=startBlock;
-		isOccupied= -1; // -1 means unoccupied
-	}
-	
-	GridBlock(int x, int y, boolean startBlock) throws InterruptedException {
-		this(startBlock);
-		coords = new int [] {x,y};
-	}
-	
-	public   int getX() {return coords[0];}  
-	
-	public   int getY() {return coords[1];}
-
-	// Method to attempt to occupy the block
-	public synchronized boolean occupy(int swimmerID) {
-		if (isOccupied==-1) { // block is not occupied
-			isOccupied=swimmerID; // occupy the block
-			return true;
-		}
-		return false; // block is occupied
-	}
-	
-	
-	
-	//Get a block
-	public  boolean get(int threadID) throws InterruptedException {
-		if (isOccupied==threadID) return true; //thread Already in this block
-		if (isOccupied>=0) return false; //space is occupied
-		isOccupied= threadID;  //set ID to thread that had block
-		return true;
-	}
-		
-	
-	//release a block
-	public  void release() {
-		isOccupied= -1; // Mark
-	}
-	
-
-	//is a bloc already occupied?
-	public  boolean occupied() {
-		if(isOccupied==-1) return false;
-		return true;
-	}
-	
-	
-	//is a start block
-	public  boolean isStart() {
-		return isStart;	
-	}
-
+    
+    private int isOccupied; // ID of the swimmer occupying the block, -1 if none
+    private final boolean isStart;  // is this a starting block?
+    private int[] coords; // the coordinate of the block.
+    
+    // Constructor for start block
+    GridBlock(boolean startBlock) throws InterruptedException {
+        isStart = startBlock;
+        isOccupied = -1; // -1 means unoccupied
+    }
+    
+    // Constructor with coordinates
+    GridBlock(int x, int y, boolean startBlock) throws InterruptedException {
+        this(startBlock);
+        coords = new int[]{x, y};
+    }
+    
+    // Get the X coordinate
+    public int getX() {
+        return coords[0];
+    }
+    
+    // Get the Y coordinate
+    public int getY() {
+        return coords[1];
+    }
+    
+    // Method to attempt to occupy the block
+    public synchronized boolean occupy(int swimmerID) {
+        if (isOccupied == -1) { // block is not occupied
+            isOccupied = swimmerID; // occupy the block
+            return true;
+        }
+        return false; // block is occupied
+    }
+    
+    // Release the block
+    public synchronized void release() {
+        isOccupied = -1; // Mark block as unoccupied
+    }
+    
+    // Check if the block is occupied
+    public synchronized boolean isOccupied() {
+        return isOccupied != -1;
+    }
+    
+    // Check if this is a starting block
+    public boolean isStart() {
+        return isStart;
+    }
 }
